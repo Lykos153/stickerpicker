0198bdcfc6ad4c8cb9ce52ea9606a73b9062492a
Silvio Ankermann <silvio@booq.org>
2022-02-13 02:50:11 +0100

    Begin user_widget.py

---
+++ sticker/user_widget.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import datetime
+import requests
+import os
+
+def get_users(homeserver, admin_token):
+    next_token = 0
+    while True:
+        jret = requests.get(
+            f"{homeserver}/_synapse/admin/v2/users",
+            # headers = header,
+            params= {
+                "access_token": admin_token,
+                "from": next_token,
+                "limit": 10,
+                "guests": "true"
+            }
+        ).json()
+        for user in jret['users']:
+            yield user['name']
+        if 'next_token' in jret:
+            next_token = jret['next_token']
+        else:
+            return
+
+def get_login_token(homeserver, user, admin_token):
+    expire_at = datetime.datetime.now() + datetime.timedelta(days=1)
+    res = requests.post(
+        f"{homeserver}/_synapse/admin/v1/users/{user}/login",
+        params={'access_token': admin_token},
+        json={'valid_until_ms': int(expire_at.timestamp()*1000)}
+    )
+    try:
+        token = res.json()['access_token']
+    except KeyError:
+        raise Exception(f"Could not get token for {user}", res.json())
+    else:
+        return token
+
+def get_m_widgets(homeserver, user, token):
+    res = requests.get(
+        f"{homeserver}/_matrix/client/v3/user/{user}/account_data/m.widgets",
+        params={'access_token': token}
+    )
+    return res.json()
+
+def set_m_widgets(homeserver, user, token, url, erase=False):
+
+    account_widgets = get_m_widgets(homeserver, user, token)
+    if erase:
+        account_widgets = {}
+    account_widgets['stickerpicker'] = \
+    {
+        "content": {
+            "type": "m.stickerpicker",
+            "url": url,
+            "name": "Stickerpicker",
+            "data": {}
+        },
+        "sender": user,
+        "state_key": "stickerpicker",
+        "type": "m.widget",
+        "id": "stickerpicker"
+    }
+    res = requests.put(
+        f"{homeserver}/_matrix/client/v3/user/{user}/account_data/m.widgets",
+        params={'access_token': token},
+        json = account_widgets
+    )
+    return res.status_code == 200
+
+def set_m_widgets_users(homeserver, users, erase=False):
+    print(f"Setting widgets...")
+    for user in users:
+        print(f"  {user}")
+        token = get_login_token(homeserver, user)
+        set_m_widgets(homeserver, user, token, sticker_url, erase=erase)
